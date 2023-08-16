e43c14c0242e7142fff523783cccac3cede31c34
Caro72 <106418089+Caro72@users.noreply.github.com>
2023-08-16 06:40:43 +0000

    new change

--- src/models.py
+++ src/models.py
@@ -7,57 +7,41 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Planet(Base):
-    __tablename__ = 'planet'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False)
-    climate = Column(String(100))
-    terrain = Column(String(100))
-    population = Column(Integer)
-    # Add more attributes as needed
-    residents = relationship('Character', secondary='planet_residents')
+class Usuario(Base):
+    __tablename__ = 'usuario'
+    UsuarioID = Column(Integer, primary_key=True)
+    Nombre = Column(String(100))
+    Email = Column(String(100))
+    Contraseña = Column(String(100))
+    favoritos = relationship('Favorito', back_populates='usuario')
 
-class Character(Base):
-    __tablename__ = 'character'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False)
-    birth_year = Column(String(20))
-    gender = Column(String(20))
-    homeworld_id = Column(Integer, ForeignKey('planet.id'))
-    homeworld = relationship('Planet', back_populates='residents')
-    starships = relationship('Starship', secondary='character_starships')
-    vehicles = relationship('Vehicle', secondary='character_vehicles')
+class Planeta(Base):
+    __tablename__ = 'planeta'
+    PlanetaID = Column(Integer, primary_key=True)
+    Nombre = Column(String(100))
+    Clima = Column(String(100))
+    Terreno = Column(String(100))
+    Poblacion = Column(Integer)
+    favoritos = relationship('Favorito', back_populates='planeta')
 
-class Starship(Base):
-    __tablename__ = 'starship'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False)
-    model = Column(String(100))
-    starship_class = Column(String(100))
-    pilots = relationship('Character', secondary='starship_pilots')
+class Personaje(Base):
+    __tablename__ = 'personaje'
+    PersonajeID = Column(Integer, primary_key=True)
+    Nombre = Column(String(100))
+    AñoNacimiento = Column(String(20))
+    Género = Column(String(20))
+    favoritos = relationship('Favorito', back_populates='personaje')
 
-class Vehicle(Base):
-    __tablename__ = 'vehicle'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False)
-    model = Column(String(100))
-    vehicle_class = Column(String(100))
-    pilots = relationship('Character', secondary='vehicle_pilots')
+class Favorito(Base):
+    __tablename__ = 'favorito'
+    FavoritoID = Column(Integer, primary_key=True)
+    UsuarioID = Column(Integer, ForeignKey('usuario.UsuarioID'))
+    PlanetaID = Column(Integer, ForeignKey('planeta.PlanetaID'))
+    PersonajeID = Column(Integer, ForeignKey('personaje.PersonajeID'))
 
-class PlanetResidents(Base):
-    __tablename__ = 'planet_residents'
-    planet_id = Column(Integer, ForeignKey('planet.id'), primary_key=True)
-    character_id = Column(Integer, ForeignKey('character.id'), primary_key=True)
-
-class CharacterStarships(Base):
-    __tablename__ = 'character_starships'
-    character_id = Column(Integer, ForeignKey('character.id'), primary_key=True)
-    starship_id = Column(Integer, ForeignKey('starship.id'), primary_key=True)
-
-class CharacterVehicles(Base):
-    __tablename__ = 'character_vehicles'
-    character_id = Column(Integer, ForeignKey('character.id'), primary_key=True)
-    vehicle_id = Column(Integer, ForeignKey('vehicle.id'), primary_key=True)
+    usuario = relationship('Usuario', back_populates='favoritos')
+    planeta = relationship('Planeta', back_populates='favoritos')
+    personaje = relationship('Personaje', back_populates='favoritos')
 
     def to_dict(self):
         return {}
